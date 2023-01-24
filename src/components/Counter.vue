45cdf25bb580372caeff26717f98d880072c6372
geekhall <174821300@qq.com>
2023-01-24 12:43:42 +0800

    chore: add counter vue page

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="counter">
+    <h3>Counter</h3>
+    <hr />
+    <h2>{{ cnt }}</h2>
+    <p>Counter page</p>
+    <el-button type="primary" @click="increase">Increase</el-button>
+    <el-button type="primary" @click="decrease">Decrease</el-button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
+
+const cnt = computed(() => store.state.count);
+const increase = () => store.commit("increment", { amount: 2 });
+const decrease = () => store.commit("decrement", { amount: 2 });
+</script>
+
+<style lang="less" scoped>
+.counter {
+  width: 100%;
+  height: 100px;
+}
+</style>
